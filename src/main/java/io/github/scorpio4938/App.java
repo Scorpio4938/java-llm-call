5edceb01d484214abefca472123bfdd7b9972e8b
Scorpio493.8 <Scorpio493.8@gmail.com>
2025-02-08 11:35:24 +0800

    init with maven

---
+++ src/main/java/io/github/scorpio4938/App.java
@@ -0,0 +1,13 @@
+package io.github.scorpio4938;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
